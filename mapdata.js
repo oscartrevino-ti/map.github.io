36447b2e8f8edb329b36089262ee03e955d87e16
Oscar Treviño <44791985+oscartrevino-ti@users.noreply.github.com>
2023-10-13 15:59:15 -0600

    commit

---
+++ mapdata.js
@@ -0,0 +1,217 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#ffffff",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#cfcfcf",
+    state_hover_color: "#cfcfcf",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "yes",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FD5468",
+    location_opacity: "2",
+    location_hover_opacity: 1,
+    location_size: "0",
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "#FFFFFF",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    MEX2706: {
+      name: "Baja California",
+      description: " "
+    },
+    MEX2707: {
+      description: " "
+    },
+    MEX2708: {
+      name: "Saltillo",
+      url: "https://www.google.com.mx/maps/place/Perif.+Luis+Echeverr%C3%ADa+1800-b,+Av%C3%ADcola,+25280+Saltillo,+Coah./@25.4313072,-100.9741715,17z/data=!3m1!4b1!4m5!3m4!1s0x86880d50f9eb118f:0xe136276386ef6ac5!8m2!3d25.4313072!4d-100.9715966?entry=ttu",
+      inactive: "no",
+      hover_color: "#F26E0F",
+      description: "(844) 143 88 95 <br><br>Perif. Luis Echeverría 1800 - B, Avícola, 25290 Saltillo, Coah., Mexico",
+      color: "#f48a3e"
+    },
+    MEX2709: {
+      name: "Chihuahua",
+      description: " "
+    },
+    MEX2710: {
+      name: "Durango",
+      description: " "
+    },
+    MEX2711: {
+      name: "Sinaloa",
+      description: " "
+    },
+    MEX2712: {
+      name: "Sonora",
+      description: " "
+    },
+    MEX2713: {
+      name: "Zacatecas",
+      description: " "
+    },
+    MEX2714: {
+      name: "Nuevo León",
+      description: " "
+    },
+    MEX2715: {
+      name: "San Luis Potosí",
+      description: " "
+    },
+    MEX2716: {
+      name: "Tamaulipas",
+      description: " "
+    },
+    MEX2717: {
+      name: "Aguascalientes",
+      description: " "
+    },
+    MEX2718: {
+      name: "Colima",
+      description: " "
+    },
+    MEX2719: {
+      name: "Jalisco",
+      description: " "
+    },
+    MEX2720: {
+      name: "Michoacán",
+      description: " "
+    },
+    MEX2721: {
+      name: "Nayarit",
+      description: " "
+    },
+    MEX2722: {
+      name: "Campeche",
+      description: " "
+    },
+    MEX2723: {
+      name: "Oaxaca",
+      description: " "
+    },
+    MEX2724: {
+      name: "Puebla",
+      description: " "
+    },
+    MEX2725: {
+      name: "Tabasco",
+      description: " "
+    },
+    MEX2726: {
+      name: "Tlaxcala",
+      description: " "
+    },
+    MEX2727: {
+      name: "Distrito Federal",
+      description: " "
+    },
+    MEX2728: {
+      name: "Guanajuato",
+      description: " "
+    },
+    MEX2729: {
+      name: "Guerrero",
+      description: " "
+    },
+    MEX2730: {
+      name: "Hidalgo",
+      description: " "
+    },
+    MEX2731: {
+      name: "México",
+      description: " "
+    },
+    MEX2732: {
+      name: "Morelos",
+      description: " "
+    },
+    MEX2733: {
+      name: "Querétaro",
+      description: " "
+    },
+    MEX2734: {
+      name: "Veracruz",
+      description: " "
+    },
+    MEX2735: {
+      name: "Chiapas",
+      description: " "
+    },
+    MEX2736: {
+      name: "Quintana Roo",
+      description: " "
+    },
+    MEX2737: {
+      name: "Yucatán",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      lat: "25.438306",
+      lng: "-100.973730",
+      name: "Saltillo"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
