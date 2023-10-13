31861bb8e35b5f7270aa81ea1d0feb4607291efd
Oscar Treviño <44791985+oscartrevino-ti@users.noreply.github.com>
2023-10-13 16:06:51 -0600

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
    //General settings
-    width: "300", //'700' or 'responsive'
+    width: "700", //'700' or 'responsive'
     background_color: "#ffffff",
     background_transparent: "yes",
     border_color: "#ffffff",
